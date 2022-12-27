10a82b38d4cf4103f17cccf5f8ebe6a3e9c6f0be
karimkohel <kareemkohel@gmail.com>
2022-12-27 16:39:15 +0200

    implemented base station class

---
+++ baseStation.py
@@ -0,0 +1,45 @@
+from state import State
+from mobileStation import MobileStation
+from packet import Packet
+
+class BaseStation():
+
+    # still need to implement wait delay
+
+    def __init__(self):
+        self.busy = False
+        self.state = State.IDLE
+        self.currSender = None
+
+
+    def activeAction(self):
+        if self.state == State.IDLE:
+            return
+        elif self.state == State.SIFS_before_emitCTS:
+            # delay SIFS_before_emitCTS: 3
+            self.state = State.emitCTS
+
+        elif self.state == State.emitCTS:
+            # delay SIFS_before_emitCTS: 15
+            # send your CTS using reception function
+            self.state = State.SIFS_before_rcvPKT
+
+        elif self.state == State.SIFS_before_rcvPKT:
+            # delay SIFS_before_rcvPKT: 3
+            self.state = State.rcvPKT
+
+        elif self.state == State.rcvPKT:
+            # delay rcvPKT: 61
+            self.state = State.IDLE
+
+        elif self.state == State.SIFS_before_emitACK:
+            # delay SIFS_before_emitACK : 3
+            self.state = State.emitACK
+
+        elif self.state == State.emitACK:
+            # send your ACK using reception function
+            self.state = State.IDLE
+
+
+    def receptionAction(self, packet):
+        if (self.state == State.IDLE) and (not packet.corrupted) and (packet.type == Packet.RTS):
